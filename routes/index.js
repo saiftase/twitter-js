f151bca57a54b831ae565ef9d95cee0366a0a87b
Saif Tase <saiftase@gmail.com>
2016-02-04 01:36:32 -0500

    Post routing by ID

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,10 @@ router.get('/users/:name', function(req, res) {
   res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list } );
 });
 
+router.get('/tweets/:id', function(req, res){
+	var id = parseInt(req.params.id);
+	var tweet = tweetBank.find( {id:id} );
+	res.render( 'index', { title: 'Twitter.js - Post ID:' + id, tweets: tweet } );
+});
+
 module.exports = router;
