ce0ee3f21c17b634d155d88948028b70e847aad4
Saif Tase <saiftase@gmail.com>
2016-02-04 01:49:57 -0500

    Added post route

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ var express = require ('express');
 var app = express();
 var swig = require('swig');
 var routes = require('./routes')
+var bodyParser = require('body-parser');
 
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 swig.setDefaults({cache: false});
 
+//Logging
 app.use(function(req, res, next){
 	console.log(req.method, req.url, res.statusCode);
 	next();
 });
 
+//Parsing
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+//Static & Dynamic Routes
 app.use(express.static('public'));
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,13 @@ tweetBank.add("Scott", "New Tweet");
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true} );
 });
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var list = tweetBank.find( {name: name} );
-  res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list } );
+  res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list, showForm: true, name: name} );
 });
 
 router.get('/tweets/:id', function(req, res){
@@ -21,5 +21,13 @@ router.get('/tweets/:id', function(req, res){
 	var tweet = tweetBank.find( {id:id} );
 	res.render( 'index', { title: 'Twitter.js - Post ID:' + id, tweets: tweet } );
 });
+
+router.post('/tweets', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
+
 
 module.exports = router;
