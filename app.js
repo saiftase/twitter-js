e0a7ea940fbdd677dd6ce76618bf8aaa879340a2
Saif Tase <saiftase@gmail.com>
2016-02-03 22:08:54 -0500

    Middleware + build scripts

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require ('express');
 var app = express();
 
+app.use("/special/", function(req, res, next){
+	console.log("You've reached the special area.");
+	next();
+})
+
+app.use(function(req, res, next){
+	console.log(req.method, req.url, res.statusCode);
+	next();
+});
+
 app.get('/', function(req, res){
 	res.send("Welcome");
 });
